5529c433c59dd66e670340b71191d37d09e1c86b
Alessandro Coppellotti <coppellottialessandro@gmail.com>
2026-07-21 17:57:53 +0200

    test

---
+++ learning/include/pcg_basic.h
@@ -0,0 +1,11 @@
+--2026-07-21 09:19:28--  https://raw.githubusercontent.com/imneme/pcg-c-basic/master/pcg_basic.h
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.109.133, 185.199.108.133, 185.199.111.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.109.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 2520 (2.5K) [text/plain]
+Saving to: ‘pcg_basic.h’
+
+     0K ..                                                    100% 23.5M=0s
+
+2026-07-21 09:19:28 (23.5 MB/s) - ‘pcg_basic.h’ saved [2520/2520]
+
---
+++ learning/src/pcg_basic.c
@@ -0,0 +1,11 @@
+--2026-07-21 09:19:51--  https://raw.githubusercontent.com/imneme/pcg-c-basic/master/pcg_basic.c
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.111.133, 185.199.110.133, 185.199.108.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.111.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3744 (3.7K) [text/plain]
+Saving to: ‘pcg_basic.c’
+
+     0K ...                                                   100% 70.0M=0s
+
+2026-07-21 09:19:52 (70.0 MB/s) - ‘pcg_basic.c’ saved [3744/3744]
+
