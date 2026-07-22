efe034ef60ca49d0b337954370498715ebfcedb6
Alessandro Coppellotti <coppellottialessandro@gmail.com>
2026-07-22 12:49:49 +0200

    Added random_generator.c

--- learning/src/random_number.c
+++ learning/src/random_number.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <time.h>
 #include "pcg_basic.h"
+
+
+int main(void){
+
+    uint64_t seed = (uint64_t)time(NULL);
+    uint64_t stream = 54u;
+
+    // pcg_srandom(seed, stream indipendente)
+    pcg32_srandom(seed, stream);
+
+    // 4294967296.0 = 2^32
+    double x = (double)pcg32_random() / 4294967296.0;
+
+    printf("Seed: %ld\nNumber: %f\n", seed, x);
+    return 0;
+}
